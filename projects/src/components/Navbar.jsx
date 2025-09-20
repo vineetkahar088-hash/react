d0876e40b10a886bfe6337e32bca9a354a2fc9cc
vineet <vineetkahar088@gmail.com>
2025-09-20 10:56:07 -0700

    Add files via upload

---
+++ projects/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Nvabar() {
+    return( 
+        <div>
+            {/* left side */}
+            <div>
+                {/*logo*/}
+                <img src="" alt="" />
+                
+            </div>
+
+        </div>
+    )
+}
